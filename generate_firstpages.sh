7178561d271dd685f73eaae907a754b950af281e
Matthew Bradbury <matt-bradbury@live.co.uk>
2021-03-28 14:47:29 +0100

    Add presentation first pages

--- generate_firstpages.sh
+++ generate_firstpages.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir -p firstpages
+mkdir -p presentations-firstpages
+
 for paper in papers/*.pdf
 do
 	target="firstpages/$(basename $paper)"
@@ -7,5 +10,15 @@ do
 
 	echo "$paper -> $target"
 
-	gs -o $target -sDEVICE=png16m -r300 -dDownScaleFactor=3 -dFirstPage=1 -dPDFLastPage=1 $paper
+	gs -o "$target" -sDEVICE=png16m -r300 -dDownScaleFactor=3 -dFirstPage=1 -dPDFLastPage=1 "$paper"
+done
+
+for paper in presentations/*.pdf
+do
+	target="presentations-firstpages/$(basename $paper)"
+	target=${target//pdf/png}
+
+	echo "$paper -> $target"
+
+	gs -o "$target" -sDEVICE=png16m -r300 -dDownScaleFactor=3 -dFirstPage=1 -dPDFLastPage=1 "$paper"
 done
